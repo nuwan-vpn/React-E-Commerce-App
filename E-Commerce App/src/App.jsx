b300bdb483939e0a38de2603939b48d0465cfe7e
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-15 23:10:47 +0530

    feat: Import components

--- E-Commerce App/src/App.jsx
+++ E-Commerce App/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-        <Navbar/>
-      
+      <Navbar/>
+      <ProductList/>
+      <Details/>
+      <Cart/>
+      <Default/>
     </>
   )
 }
