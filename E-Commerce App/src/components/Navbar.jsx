4eb957780cefe4bb7a9f50a8b49ec03807d66e1c
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-17 22:33:08 +0530

    fix: correct image import and path in Navbar component
    The previous code had an issue with the image import and path, which caused the image not to be displayed correctly.
    
    This commit fixes the following:
    
    1. Changed the image source from `src='logo'` to `src={logo}` to correctly reference the imported image.
    2. Added the `alt` attribute to the `img` tag for accessibility.
    3. Ensured the image file path `'../telephone_icon.png'` is correct relative to the `Navbar.jsx` file.
    
    These changes should resolve the issue with the image not being uploaded and displayed properly.

--- E-Commerce App/src/components/Navbar.jsx
+++ E-Commerce App/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import logo from '../telephone_icon.png'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../telephone_icon.png';
+
 export default class Navbar extends Component {
   render() {
     return (
       <nav className='navbar'>
         <Link to='/'>
-          <img src='logo'/>
-          <h1>hhhhh</h1>
+          <img src={logo} alt="Logo" />
         </Link>
       </nav>
-    )
+    );
   }
 }
