4cce1365e8a037f79771922349d191e88e4a8d2d
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-19 13:49:19 +0530

    feat: Add navigation links to the navigation bar

--- E-Commerce App/src/components/Navbar.jsx
+++ E-Commerce App/src/components/Navbar.jsx
@@ -8,10 +8,15 @@ export default class Navbar extends Component {
     return (
       <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
         <Link to='/'>
-          <img src={logo} alt="Logo"  height={100} width={100}/>
-          
+          <img src={logo} alt="store"  height={60} width={60}/> 
         </Link>
-       
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
