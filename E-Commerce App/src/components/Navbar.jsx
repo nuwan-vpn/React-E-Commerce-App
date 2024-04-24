b71a5cc35396847e5676189e1e50ad0066684ab7
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-04-24 15:16:41 +0530

    Feat: Add cart icon and position it on the right side
    This commit adds the following changes:
    
    - Install and import necessary packages for Font Awesome icons:
      - @fortawesome/fontawesome-svg-core
      - @fortawesome/free-solid-svg-icons
      - @fortawesome/react-fontawesome
    
    - Import the FontAwesomeIcon component and faCartPlus icon from Font Awesome
    
    - Render the FontAwesomeIcon component with the faCartPlus icon inside the cart Link component
    
    - Wrap the navbar content inside a container-fluid div for proper alignment
    
    - Use Bootstrap utility classes (me-auto, navbar-text) to position the cart icon on the right side
    
    - Remove unnecessary classes and adjust spacing/alignment of other navbar elements
    
    With these changes, the cart icon is now displayed on the right side of the navbar, providing a clear visual indicator for the user's cart.

--- E-Commerce App/src/components/Navbar.jsx
+++ E-Commerce App/src/components/Navbar.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../telephone_icon.png';
-// import '../App.css';
+import '../App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
-        <Link to='/'>
-          <img src={logo} alt="store"  height={60} width={60}/> 
-        </Link>
-        <ul className="navbar-nav align-items-center">
-          <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
-              products
-            </Link>
-          </li>
-        </ul>
+        <div className="container-fluid">
+          <Link to='/' className='navbar-brand'>
+            <img src={logo} alt="store" height={60} width={60} />
+          </Link>
+          <ul className="navbar-nav me-auto align-items-center">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                products
+              </Link>
+            </li>
+          </ul>
+          <Link to='/cart' className='navbar-text'>
+            <button className='btn btn-light'>
+              <FontAwesomeIcon icon={faCartPlus} /> My Cart
+            </button>
+          </Link>
+        </div>
       </nav>
     );
   }
