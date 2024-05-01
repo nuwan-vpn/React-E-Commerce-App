7f7098071b37023f73e22a590afaa4337940cc39
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-01 22:49:39 +0530

    Render Cart component with conditional rendering based on cart items

--- E-Commerce App/src/components/Cart.jsx
+++ E-Commerce App/src/components/Cart.jsx
@@ -3,9 +3,25 @@ import React, { Component } from 'react'
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <section>
+      <ProductConsumer>
+        {value => {
+          const { cart } = value;
+          if (cart.length > 0) {
+            return (
+              <React.Fragment>
+                <Title name="your" title="cart" />
+                <CartColumns />
+                <CartList value={value} />
+                <CartTotals value={value} history={this.props.history} />
+              </React.Fragment>
+            );
+          } else {
+            return <EmptyCart />;
+          }
+        }}
+      </ProductConsumer>
+    </section>
     )
   }
 }
