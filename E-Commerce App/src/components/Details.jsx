e045e7e012f74a5a5e69821ebded10c2f2948a08
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-04 21:34:24 +0530

    Add product image to Details component

--- E-Commerce App/src/components/Details.jsx
+++ E-Commerce App/src/components/Details.jsx
@@ -22,7 +22,11 @@ export default class Details extends Component {
                 <h1>{title}</h1>
               </div>
             </div>
-            
+             {/* end of title */}
+             <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="" />
+                </div>
             </div>
           )
         }}
