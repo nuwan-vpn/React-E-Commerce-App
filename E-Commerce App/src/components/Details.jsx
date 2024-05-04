fa4214572a29404208bfb217922ae3f4f839aafc
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-04 21:44:16 +0530

    Fix closing tags in Details component

--- E-Commerce App/src/components/Details.jsx
+++ E-Commerce App/src/components/Details.jsx
@@ -7,32 +7,23 @@ export default class Details extends Component {
   render() {
     return (
       <ProductConsumer>
-         {value => {
-          const {
-            id,
-            company,
-            img,
-            info,
-            price,
-            title,
-            inCart
-          } = value.detailProduct;
+        {value => {
+          const { id, company, img, info, price, title, inCart } = value.detailProduct;
           return (
             <div className="container py-5">
-            {/* title */}
-            <div className="row">
-              <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                <h1>{title}</h1>
+              {/* title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
               </div>
-            </div>
-            {/* end of title */}
-            <div className="row">
-              <div className="col-10 mx-auto col-md-6 my-3">
-                <img src={img} className="img-fluid" alt="" />
-              </div>
-            </div>
-             {/* prdoduct info */}
-             <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+              {/* end of title */}
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="" />
+                </div>
+                {/* product info */}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                   <h1>model : {title}</h1>
                   <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
                     made by : <span className="text-uppercase">{company}</span>
@@ -47,29 +38,28 @@ export default class Details extends Component {
                     some info about product :
                   </p>
                   <p className="text-muted lead">{info}</p>
-                  </div>
-               {/* buttons */}
-               <div>
-                    <Link to="/">
-                      <ButtonContainer>back to products</ButtonContainer>
-                    </Link>
-                    <ButtonContainer
-                      cart
-                      disabled={inCart ? true : false}
-                      onClick={() => {
-                        value.addToCart(id);
-                        value.openModal(id);
-                      }}
-                    >
-                      {inCart ? "in cart" : "add to cart"}
-                    </ButtonContainer>
-                  </div>
+                </div>
+                {/* buttons */}
+                <div>
+                  <Link to="/">
+                    <ButtonContainer>back to products</ButtonContainer>
+                  </Link>
+                  <ButtonContainer
+                    cart
+                    disabled={inCart ? true : false}
+                    onClick={() => {
+                      value.addToCart(id);
+                      value.openModal(id);
+                    }}
+                  >
+                    {inCart ? "in cart" : "add to cart"}
+                  </ButtonContainer>
                 </div>
               </div>
             </div>
-          )
+          );
         }}
       </ProductConsumer>
-    )
+    );
   }
 }
