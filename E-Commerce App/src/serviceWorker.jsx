3f3bffd3fff4a009030903c3ff09337365b2a435
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-05-07 22:17:26 +0530

    feat: add service worker unregistration utility

--- E-Commerce App/src/serviceWorker.jsx
+++ E-Commerce App/src/serviceWorker.jsx
@@ -85,3 +85,11 @@ function registerValidSW(swUrl, config) {
         console.error('Error during service worker registration:', error);
       });
   }
+
+export function unregister() {
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+    registration.unregister();
+    });
+}
+}
